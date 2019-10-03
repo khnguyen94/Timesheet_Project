2fe1cd276e9540e57e47e2364ee5d46d22205df5
msheffield <msheffield@adaptivebiotech.com>
2019-10-02 20:35:36 -0700

    add to table function written and add button clear input fixed

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -14,15 +14,52 @@ firebase.initializeApp(firebaseConfig);
 // 
 var database = firebase.database();
 
+function addToTable(name, role, startdate, monthlyrate) {
+    let row = $("<tr>");
+
+    let employeeName = $("<td>");
+    employeeName.text(name);
+    row.append(employeeName);
+
+    let employeeRole = $("<td>");
+    employeeRole.text(role);
+    row.append(employeeRole);
+
+    let employeeStartdate = $("<td>");
+    employeeStartdate.text(startdate);
+    row.append(employeeStartdate);
+
+    let employeeMonthsworked = $("<td>");
+    employeeMonthsworked.text()
+    row.append(employeeMonthsworked);
+
+    let employeeMonthlyrate = $("<td>");
+    employeeMonthlyrate.text(monthlyrate);
+    row.append(employeeMonthlyrate);
+
+    let employeeInvoicetotal = $("<td>");
+    employeeInvoicetotal.text();
+    row.append(employeeInvoicetotal);
+
+}
 
 $("#new-employee-submit").on("click", function (event) {
     event.preventDefault();
-
 
     name = $("#new-employee-name").val().trim();
     role = $("#new-employee-role").val();
     startdate = $("#new-employee-start-date").val();
     monthlyrate = $("#new-employee-monthly-rate").val();
+
+    $("#new-employee-name").val("");
+    $("#new-employee-role").val("");
+    $("#new-employee-start-date").val("");
+    $("#new-employee-monthly-rate").val("");
+
+    console.log(name);
+    console.log(role);
+    console.log(startdate);
+    console.log(monthlyrate);
 
     // Don't forget to provide initial data to your Firebase database.
     database.ref().push({
