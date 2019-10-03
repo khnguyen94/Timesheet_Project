eca541ba9d2afd867e3ec0842be94f354cb2cadf
msheffield <msheffield@adaptivebiotech.com>
2019-10-02 19:36:59 -0700

    firebase config

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyBC6lMeFL8PYTyVFKONgfcxhKCNklTSB9k",
+    authDomain: "timesheet-kmj.firebaseapp.com",
+    databaseURL: "https://timesheet-kmj.firebaseio.com",
+    projectId: "timesheet-kmj",
+    storageBucket: "timesheet-kmj.appspot.com",
+    messagingSenderId: "888485096272",
+    appId: "1:888485096272:web:73d9a30aa2e44ef0489a55"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
