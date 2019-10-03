b3d0802d31d469fee6066a566e8cb0ee6a12c6c1
msheffield <msheffield@adaptivebiotech.com>
2019-10-02 19:54:04 -0700

    firebase added and new employee entry function made

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,6 +7,29 @@ var firebaseConfig = {
     storageBucket: "timesheet-kmj.appspot.com",
     messagingSenderId: "888485096272",
     appId: "1:888485096272:web:73d9a30aa2e44ef0489a55"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// 
+var database = firebase.database();
+
+
+
+$("#new-employee-submit").on("click", function (event) {
+    event.preventDefault();
+
+
+    name = $("#new-employee-name").val().trim();
+    role = $("#new-employee-role").val().trim();
+    startdate = $("#email-start-date").val().trim();
+    monthlyrate = $("#email-monthly-rate").val().trim();
+
+    // Don't forget to provide initial data to your Firebase database.
+    database.ref().push({
+      name: name,
+      role: role,
+      startdate: startdate,
+      monthlyrate: monthlyrate,
+    });
+  });
